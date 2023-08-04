8c19cae119a4be1e87d732a577673016e3fcf374
Job Henandez Lara <hj93@protonmail.com>
2023-08-04 00:32:48 -0700

    add matrix arithmetic

--- src/compiler/interpreter.py
+++ src/compiler/interpreter.py
@@ -27,31 +27,39 @@ def evaluate(ast):
                     matrix_entries = get_matrix_elements(elements)
                     return matrix_entries
 
-                case Sum(e1,e2):
-                    vec = get_vector_elements(e1.elements)
-                    vec2 = get_vector_elements(e2.elements)
+                case Sum(Vec(elements),Vec(elements2)):
+                    vec = get_vector_elements(elements)
+                    vec2 = get_vector_elements(elements2)
                     vec_object = V(vec)
                     vec2_object = V(vec2)
                     return vec_object + vec2_object
 
-                case Minus(e1,e2):
-                    vec = get_vector_elements(e1.elements)
-                    vec2 = get_vector_elements(e2.elements)
+                case Minus(Vec(elements), Vec(elements2)):
+                    vec = get_vector_elements(elements)
+                    vec2 = get_vector_elements(elements2)
                     vec_object = V(vec)
                     vec2_object = V(vec2)
                     return vec_object - vec2_object
 
-                case Product(e1,e2):
-                    operand = V(get_vector_elements(e1.elements)) if isinstance(e1, Vec) else e1.num
+                case Product(Vec(elements), e2):
+                    operand = V(get_vector_elements(elements)) 
                     operand2 = V(get_vector_elements(e2.elements)) if isinstance(e2, Vec) else e2.num
             
                     return operand * operand2
 
-        case Vec(elements):
-            vector_entries = get_vector_elements(elements)
-            return V(vector_entries)
+                case Sum(Matrix(e1), Matrix(e2)):
+                    mat = Mat(get_matrix_elements(e1))
+                    mat2 = Mat(get_matrix_elements(e2))
+                    return mat + mat2
 
-        case Matrix(elements):
-            matrix_elements = get_matrix_elements(elements)
-            return Mat(matrix_elements)
+                case Minus(Matrix(e1), Matrix(e2)):
+                    mat = Mat(get_matrix_elements(e1))
+                    mat2 = Mat(get_matrix_elements(e2))
+                    return mat - mat2
 
+                case Product(Matrix(e1), e2):
+                    operand = Mat(get_matrix_elements(e1))
+                    operand2 = Mat(get_matrix_elements(e2)) if isinstance(e2, Matrix) else e2.num
+                    return operand * operand
+
+        
