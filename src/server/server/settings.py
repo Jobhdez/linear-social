7011127774520c4ad2f40902461f4aeb0cb6f68e
Job <hj93@protonmail.com>
2023-08-05 18:39:57 -0700

    configure app

--- src/server/server/settings.py
+++ src/server/server/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
+AUTH_USER_MODEL = 'api.User'
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-@83n+$fh4$ofpz^fk-+s^g@ovf_v9w7)6$0jpfq0*a3o9k83lj'
 
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'rest_framework',
+    
 ]
 
 MIDDLEWARE = [
