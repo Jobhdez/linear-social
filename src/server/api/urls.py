905e8ae43e9bc3891c46c08b047292d8de41d610
Job <hj93@protonmail.com>
2023-08-05 18:44:04 -0700

    add api urls

---
+++ src/server/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from api import views
+from django.urls import path
+
+app_name = 'api'
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('request/', views.request_friend, name='request'),
+    path('accept/', views.accept_friend_request, name='accept'),
+    path('computeAlg/', views.compute_lalg_expression, name='lalg'),
+    ]
