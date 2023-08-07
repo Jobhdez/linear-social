059074fa53c5ff0af2a520c92ca29534d1559e78
Job Henandez Lara <hj93@protonmail.com>
2023-08-07 09:24:16 -0700

    Update tasks.py

--- src/server/api/tasks.py
+++ src/server/api/tasks.py
@@ -8,11 +8,11 @@ def friend_request_sent(from_username, to_username):
     request."""
     from_user = User.objects.get(username=from_username)
     to_user = User.objects.get(username=to_username)
-    user = FriendRequest.objects.get(from_user=from_user, to_user=to_user)
+    friend_connection = FriendRequest.objects.get(from_user=from_user, to_user=to_user)
     subject = f'You have received a friend request'
-    message = f'Dear {user.to_user.first_name}, \n\n You have received a friend request from {user.from_user.first_name}. \n\n Login to your account to accept your friend request.'
+    message = f'Dear {friend_connection.to_user.first_name}, \n\n You have received a friend request from {friend_connection.from_user.first_name}. \n\n Login to your account to accept your friend request.'
     mail_sent = send_mail(subject,
                           message,
                           'jobhdezlara93@gmail.com',
-                          [user.to_user.email])
+                          [friend_connection.to_user.email])
     return mail_sent
