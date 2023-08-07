1bd60540cbaff2db849d2f78a56d8ff303c0fc7c
Job Henandez Lara <hj93@protonmail.com>
2023-08-07 04:32:40 -0700

    add email config

--- src/server/server/settings.py
+++ src/server/server/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,6 +29,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'jobhdezlara93@gmail.com'
+EMAIL_HOST_PASSWORD = str(os.getenv('GMAIL_PASSWORD'))
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 # Application definition
 
