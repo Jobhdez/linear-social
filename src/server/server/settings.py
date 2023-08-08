19c0628bcd24729e2c7db3b119ff601384e167f1
Job Henandez Lara <hj93@protonmail.com>
2023-08-08 01:36:10 -0700

    add new settings

--- src/server/server/settings.py
+++ src/server/server/settings.py
@@ -50,10 +50,12 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     'actions',
+    'debug_toolbar',
     
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -94,6 +96,12 @@ DATABASES = {
     }
 }
 
+CACHES =  {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -135,3 +143,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+    ]
