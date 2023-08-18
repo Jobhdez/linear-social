b1d22e401835e93fd44a6197499de047c277e276
Job Henandez Lara <hj93@protonmail.com>
2023-08-17 17:32:26 -0700

    Update client.py

--- src/ws_client/client.py
+++ src/ws_client/client.py
@@ -2,10 +2,10 @@ import asyncio
 import websockets
 import json
  
-async def test():
+async def chat(message):
     async with websockets.connect('ws://127.0.0.1:8000/ws/chat/room/1/') as websocket:
-        await websocket.send(json.dumps({'message': 'hello'}))
+        await websocket.send(json.dumps({'message': message}))
         response = await websocket.recv()
-        print(response)
+        return response
  
-asyncio.get_event_loop().run_until_complete(test())
+asyncio.get_event_loop().run_until_complete(chat('hello'))
